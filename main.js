1b911b51bb9fd27f36b254ca6efde071b824cc59
Patogallininin096 <113736189+Patogallininin096@users.noreply.github.com>
2023-03-17 18:43:35 -0600

    HTML.6

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 var imagen = "";
 var objeto = [];
-var status = ""; 
-function setup(){
+var status = "";
+function setup() {
     canavas = createCanvas(640, 420);
     canvas.center();
+    detector = ml5.objectDetector("cocossd", listo);
 }
-function preload(){
+function preload() {
     imagen = loadImage("dog_cat.jpg")
 }
-function draw(){
+function draw() {
     image(imagen, 0, 0, 650, 420);
-    noFill();
-    stroke("red");
-    Text();
-    rect(60, 60, 350, 350);
+    if (status != "") {
+        for (i = 0; i < objetos.legth; i++) {
+            objetoActual = objetos[i];
+            probabilidad = floor(objetoActual.confidence * 100);
+            mensaje = objetoActual.label + " " + probabilidad + "%";
+            noFill();
+            stroke("red");
+            text(mesaje, objetoActual.x + 15, objetoActual.y + 15);
+            rect(objetoActual.x, objetoActual.y, objetoActual.width, objetoActual.height);
+        }
+        document.getElementById("status").innerHTML = "Objetos detectados"
+    }
+
+}
+function listo() {
+    status = true;
+    detector.detect(imagen, obtenerResultados);
+}
+function obtenerResultados(error, resultados) {
+    if (!error) {
+        objetos = resultado;
+        console.log(resultados);
+    }
 }
